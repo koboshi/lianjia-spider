ad73dfb885ab07d33f2a9ed90924cc45beab09ab
samding <koboshi@qq.com>
2018-12-28 22:19:00 +0800

    181228开发进度

--- context/context.go
+++ context/context.go
@@ -3,10 +3,13 @@ package context
 import (
 	"errors"
 	"github.com/koboshi/mole/database"
+	"github.com/koboshi/mole/work"
 	"gopkg.in/ini.v1"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type Config struct {
@@ -30,13 +33,17 @@ type Config struct {
 }
 
 var ErrLoadConf = errors.New("could not load config file")
-var ErrConnectMysql = errors.New("could not connect mysql")
+var ErrInitMysql = errors.New("could not connect mysql")
+var ErrInitLog = errors.New("could not init logger")
 var ErrInitPool = errors.New("could not init net concurrent pool")
 
 var conf Config
-var db *database.Database
-var logger *log.Logger
-
+var Db *database.Database
+var NetRoutinePool *work.Pool
+var TraceLogger *log.Logger
+var InfoLogger *log.Logger
+var WarnLogger *log.Logger
+var ErrorLogger *log.Logger
 
 func init() {
 	//读取配置文件
@@ -53,28 +60,98 @@ func init() {
 	if err != nil {
 		panic(ErrLoadConf)
 	}
-
-	//初始化日志
-	log.SetPrefix("LOG:")
-	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
-
-	//初始化数据库连节
-
-	//初始化爬虫配置
-
-	//初始化网络配置
+	//初始化
+	initLog()
+	initMysql()
+	initNet()
+	initSpider()
 }
 
 func initLog() {
-	//TODO
+	log.SetPrefix("LOG:")
+	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
+	if conf.LogOn < 1 {
+		return
+	}
+	//初始化日志记录器
+	var logDir string
+	if len(conf.LogDir) > 0 {
+		logDir = conf.LogDir
+	}else {
+		logDir = filepath.Dir(os.Args[0]) + "/logs/"
+	}
+	var logLevelMap = make(map[string]int)
+	logLevelMap["trace"] = 1
+	logLevelMap["info"] = 2
+	logLevelMap["warn"] = 3
+	logLevelMap["error"] = 4
+	destLevel, ok := logLevelMap[strings.ToLower(conf.LogLevel)]
+	if !ok {
+		destLevel = 4
+	}
+
+	if destLevel <= logLevelMap["trace"] {
+		traceFile, err := os.OpenFile(logDir + "/trace.log", os.O_CREATE | os.O_WRONLY | os.O_APPEND, 0666)
+		if err != nil {
+			panic(ErrInitLog)
+		}
+		TraceLogger = log.New(traceFile, "Trace:", log.Ldate | log.Ltime | log.Llongfile)
+	}else {
+		TraceLogger = log.New(ioutil.Discard, "Trace:", log.Ldate | log.Ltime | log.Llongfile)
+	}
+
+	if destLevel <= logLevelMap["info"] {
+		infoFile, err := os.OpenFile(logDir + "/info.log", os.O_CREATE | os.O_WRONLY | os.O_APPEND, 0666)
+		if err != nil {
+			panic(ErrInitLog)
+		}
+		InfoLogger = log.New(infoFile, "Info:", log.Ldate | log.Ltime | log.Llongfile)
+	}else {
+		InfoLogger = log.New(ioutil.Discard, "Info:", log.Ldate | log.Ltime | log.Llongfile)
+	}
+
+	if destLevel <= logLevelMap["warn"] {
+		warnFile, err := os.OpenFile(logDir + "/warn.log", os.O_CREATE | os.O_WRONLY | os.O_APPEND, 0666)
+		if err != nil {
+			panic(ErrInitLog)
+		}
+		WarnLogger = log.New(warnFile, "Warn:", log.Ldate | log.Ltime | log.Llongfile)
+	}else {
+		WarnLogger = log.New(ioutil.Discard, "Warn:", log.Ldate | log.Ltime | log.Llongfile)
+	}
+
+	if destLevel <= logLevelMap["error"] {
+		errorFile, err := os.OpenFile(logDir + "/error.log", os.O_CREATE | os.O_WRONLY | os.O_APPEND, 0666)
+		if err != nil {
+			panic(ErrInitLog)
+		}
+		ErrorLogger = log.New(errorFile, "Error:", log.Ldate | log.Ltime | log.Llongfile)
+	}else {
+		ErrorLogger = log.New(ioutil.Discard, "Error:", log.Ldate | log.Ltime | log.Llongfile)
+	}
+
 }
 
 func initMysql() {
-	//TODO
+	host := conf.MysqlHost
+	username := conf.MysqlUser
+	password := conf.MysqlPsw
+	schema := conf.MysqlSchema
+	charset := conf.MysqlCharset
+
+	var err error
+	Db, err = database.New(host, username, password, schema, charset)
+	if err != nil {
+		panic(ErrInitMysql)
+	}
 }
 
 func initNet() {
-	//TODO
+	var err error
+	NetRoutinePool, err = work.New(conf.NetConcurrent)
+	if err != nil {
+		panic(ErrInitPool)
+	}
 }
 
 func initSpider() {
